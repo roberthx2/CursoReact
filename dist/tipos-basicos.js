02d1acb9291b4357613d45ea652047b8c829a88d
Roberth Riera <riera_1992@hotmail.com>
2018-08-29 21:20:38 -0430

    Arreglos y Emun

--- dist/tipos-basicos.js
+++ dist/tipos-basicos.js
@@ -8,3 +8,29 @@ console.log('nombreCurso: ', nombreCurso);
 console.log('contenidoCurso: ', contenidoCurso);
 var bienvenida = "Bienvenidos al curso " + nombreCurso;
 console.log('Bienvenidos', bienvenida);
+var arregloA = [1, 2, 3, 4];
+console.log('arregloA:', arregloA);
+var arregloB;
+arregloB = ['a', 'b', 'c'];
+console.log('arregloB: ', arregloB);
+//TypeScript Enums
+var DiaCurso;
+(function (DiaCurso) {
+    DiaCurso[DiaCurso["MARTES"] = 0] = "MARTES";
+    DiaCurso[DiaCurso["VIERNES"] = 1] = "VIERNES";
+})(DiaCurso || (DiaCurso = {}));
+;
+var DiasSemana;
+(function (DiasSemana) {
+    DiasSemana[DiasSemana["LUNES"] = 0] = "LUNES";
+    DiasSemana[DiasSemana["MARTES"] = 1] = "MARTES";
+    DiasSemana[DiasSemana["MIERCOLES"] = 2] = "MIERCOLES";
+    DiasSemana[DiasSemana["JUEVES"] = 3] = "JUEVES";
+    DiasSemana[DiasSemana["VIERNES"] = 4] = "VIERNES";
+})(DiasSemana || (DiasSemana = {}));
+;
+var primerDia;
+primerDia = DiaCurso.MARTES;
+console.log('primerDia:', primerDia);
+console.log('primerDia(string):', DiaCurso[0]);
+console.log('primerDia(string):', DiaCurso[DiaCurso.MARTES]);
--- src/tipos-basicos/tipos-basicos.ts
+++ src/tipos-basicos/tipos-basicos.ts
@@ -16,3 +16,19 @@ console.log('contenidoCurso: ', contenidoCurso);
 let bienvenida:string = `Bienvenidos al curso ${nombreCurso}`;
 console.log('Bienvenidos', bienvenida);
 
+let arregloA:number[] = [1, 2, 3, 4];
+console.log('arregloA:', arregloA);
+
+let arregloB: Array<string>;
+arregloB = ['a', 'b', 'c'];
+console.log('arregloB: ', arregloB);
+
+//TypeScript Enums
+enum DiaCurso {MARTES, VIERNES};
+enum DiasSemana {LUNES, MARTES, MIERCOLES, JUEVES, VIERNES};
+
+let primerDia:DiaCurso;
+primerDia = DiaCurso.MARTES;
+console.log('primerDia:', primerDia);
+console.log('primerDia(string):', DiaCurso[0]);
+console.log('primerDia(string):', DiaCurso[DiaCurso.MARTES]);
